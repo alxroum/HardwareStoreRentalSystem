7eeb547dddb74255a01b645fc71a302c6182ca7c
Alex Ro <alxroum@gmail.com>
2026-02-17 11:06:27 -0500

    created card prototype and ui structure.

--- hardware-rental/src/App.jsx
+++ hardware-rental/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import favicon from './assets/favicon.png'
 import './App.css'
 
 function App() {
@@ -9,12 +10,30 @@ function App() {
 
   return (
     <>
-      <div id="nav">
-        test nav
-      </div>
       <div id="content">
-        <div className='card'>
-          test card
+        <div id="card_001" className="card">
+
+          <div className='top-half'>
+            <img className="item-image" src={favicon}></img>
+          </div>
+          <div className='bottom-half'>
+            <div className="category">POWER TOOLS</div>
+            <div className="name">Circular Saw</div>
+            <div className="condition">Condition: Excellent</div>
+            <hr id="hr-01"></hr>
+            <div className="pricing-info">
+              <div className='left'>
+                Daily rate:<br></br>
+                Weekly rate:
+              </div>
+              <div className='right'>
+                $25.00<br></br>
+                $100.00
+              </div>
+            </div>
+            <button className="reserve-button">Reserve Now</button>
+            <button className="cart-button">Add to Cart</button>
+          </div>
         </div>
       </div>
     </>
