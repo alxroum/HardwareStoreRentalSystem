b094644063761108ac94aa467c723997735d0622
Alan Prudhomme <prudhommealan@gmail.com>
2026-02-25 15:20:53 -0500

    Added login & signup pages

--- hardware-rental/src/main.jsx
+++ hardware-rental/src/main.jsx
@@ -4,9 +4,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Login } from './pages/Login.jsx'
+import { SignUp } from './pages/SignUp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <App />
+     {/*<App />   {/* Un-comment to display default page. */}
+      <Login />       {/* Call to Login for testing purposes. */}
+      {/*<SignUp />     {/* Call to SignUp for testing purposes. */}
   </StrictMode>,
 )
