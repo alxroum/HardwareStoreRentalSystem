409d51b47a31a78f46d9cecdd289be817d4e7b4a
alexmorris90341 <alexmorris9034@gmail.com>
2026-03-12 12:49:38 -0400

    Backend Datbase connected to the website

--- hardware-rental/src/pages/Home.jsx
+++ hardware-rental/src/pages/Home.jsx
@@ -6,6 +6,16 @@ import powerWasher from '../assets/power-washer.png'
 import paintSprayer from '../assets/paint-sprayer.png'
 import chainsaw from '../assets/chainsaw.png'
 
+const imageMap = {
+    "Circular Saw": circularSaw,
+    "Power Washer": powerWasher,
+    "Paint Sprayer": paintSprayer,
+    "Chainsaw": chainsaw
+}
+
+//database
+import { useEffect, useState } from 'react'
+
 // components
 import { Card } from '../components/Card'
 
@@ -37,13 +47,26 @@ function categorize(category) {
 
 export function Home() {
 
+    const [inventory, setInventory] = useState([])
+
+   useEffect(() => {
+    fetch("http://localhost:8080/inventory")
+        .then(res => res.json())
+        .then(data => {
+            console.log("INVENTORY FROM BACKEND:", data)   // 👈 ADDED
+            setInventory(data)
+        })
+        .catch(err => console.error("Error fetching inventory:", err))
+}, [])
+
+
     return (
         <>
         <div className='two-parts'>
 
             <div id='sidebar'>
                 <table id='sidebar-table'>
-                    
+                    <tbody>
                     <tr>
                         <td><div className='category-selector'>All Categories</div></td>
                     </tr>
@@ -68,19 +91,31 @@ export function Home() {
                     <tr>
                         <td><div className='category-selector'>Demolition</div></td>
                     </tr>
+                    </tbody>
                 </table>
             </div>
     
             <div id="item-grid">
-
-                {/* Cards are defined in the following way */}
+                {/* old harcoded code
+                 Cards are defined in the following way 
                 <Card category={"POWER TOOLS"} name={"Circular Saw"} condition={"Excellent"} daily_rate={25.00} weekly_rate={100.00} image={circularSaw}/>
                 <Card category={"CLEANING"} name={"Power Washer"} condition={"Good"} daily_rate={45.00} weekly_rate={180.00} image={powerWasher}/>
                 <Card category={"PAINTING"} name={"Paint Sprayer"} condition={"Okay"} daily_rate={35.00} weekly_rate={140.00} image={paintSprayer}/>
                 <Card category={"YARD AND GARDEN"} name={"Chainsaw"} condition={"Good"} daily_rate={30.00} weekly_rate={120.00} image={chainsaw}/>
                 <Card category={"YARD AND GARDEN"} name={"Chainsaw"} condition={"Good"} daily_rate={30.00} weekly_rate={120.00} image={chainsaw}/>
                 <Card category={"YARD AND GARDEN"} name={"Chainsaw"} condition={"Good"} daily_rate={30.00} weekly_rate={120.00} image={chainsaw}/>
-
+                */}
+                {inventory.map(item => (
+                    <Card
+                        key={item.idinventory}
+                        category={item.category}
+                        name={item.equipment_name}
+                        condition={item.quality}
+                        daily_rate={item.daily_rate}
+                        weekly_rate={item.weekly_rate}
+                        image={imageMap[item.equipment_name]}
+                    />
+                ))}
 
             </div>
         </div>
